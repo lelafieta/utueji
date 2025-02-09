8d37030d8a88de1522afb621b228c7e7fd7da5a5
Lela Fieta <lelafieta11@gmail.com>
2025-02-09 01:21:52 +0100

    BottomNavigationBar

--- lib/src/features/solidary/presentation/pages/solidary_page.dart
+++ lib/src/features/solidary/presentation/pages/solidary_page.dart
@@ -1,4 +1,9 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../config/themes/app_colors.dart';
+import '../../../../core/resources/icons/app_icons.dart';
 
 class SolidaryPage extends StatefulWidget {
   const SolidaryPage({super.key});
@@ -8,10 +13,128 @@ class SolidaryPage extends StatefulWidget {
 }
 
 class _SolidaryPageState extends State<SolidaryPage> {
+  int _currentIndex = 0;
+
+  final List<String> iconPaths = [
+    AppIcons.houseChimney,
+    AppIcons.compassAlt,
+    AppIcons.handsHeart,
+    AppIcons.comments,
+  ];
+
+  final List<String> iconPathsBold = [
+    AppIcons.houseChimneyBold,
+    AppIcons.compassAltBold,
+    AppIcons.handsHeartBold,
+    AppIcons.commentsBold,
+  ];
+
+  final List<MenuItem> menuItems = [
+    MenuItem(
+        title: "Home",
+        iconPath: AppIcons.houseChimney,
+        activeIconPath: AppIcons.houseChimneyBold,
+        isActive: true),
+    MenuItem(
+      title: "Explore",
+      iconPath: AppIcons.compassAlt,
+      activeIconPath: AppIcons.compassAltBold,
+    ),
+    MenuItem(
+      title: "Favorites",
+      iconPath: AppIcons.handsHeart,
+      activeIconPath: AppIcons.handsHeartBold,
+    ),
+    MenuItem(
+      title: "Messages",
+      iconPath: AppIcons.comments,
+      activeIconPath: AppIcons.commentsBold,
+    ),
+  ];
+
+  final List<Widget> pages = [
+    const Center(child: Text("Home", style: TextStyle(fontSize: 24))),
+    const Center(child: Text("Search", style: TextStyle(fontSize: 24))),
+    const Center(child: Text("Favorites", style: TextStyle(fontSize: 24))),
+    const Center(child: Text("Profile", style: TextStyle(fontSize: 24))),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("OLA"),
+      body: pages[_currentIndex], // Alterna entre as páginas
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar.builder(
+        itemCount: menuItems.length,
+        tabBuilder: (int index, bool isActive) {
+          final menuItem = menuItems[index]; // Obtém o item do menu atual
+          final color = isActive ? AppColors.primaryColor : Colors.grey;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                menuItem.currentIconPath,
+                width: 20,
+                height: 20,
+                colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+              ),
+              const SizedBox(height: 4), // Espaçamento entre ícone e texto
+              Text(
+                menuItem.title,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: color,
+                  fontWeight: isActive ? FontWeight.bold : FontWeight.normal,
+                ),
+              ),
+            ],
+          );
+        },
+        activeIndex: _currentIndex,
+        gapLocation: GapLocation.none,
+        notchSmoothness: NotchSmoothness.softEdge,
+        onTap: (index) => setState(() {
+          _currentIndex = index;
+
+          for (int i = 0; i < menuItems.length; i++) {
+            menuItems[i] = menuItems[i].copyWith(isActive: i == index);
+          }
+        }),
+      ),
+    );
+  }
+}
+
+class MenuItem {
+  final String title;
+  final String iconPath;
+  final String activeIconPath;
+  final bool isActive;
+  final Color color;
+
+  MenuItem({
+    required this.title,
+    required this.iconPath,
+    required this.activeIconPath,
+    this.isActive = false,
+    this.color = Colors.grey,
+  });
+
+  String get currentIconPath => isActive ? activeIconPath : iconPath;
+
+  MenuItem copyWith({bool? isActive, Color? color}) {
+    return MenuItem(
+      title: title,
+      iconPath: iconPath,
+      activeIconPath: activeIconPath,
+      isActive: isActive ?? this.isActive,
+      color: color ?? this.color,
     );
   }
 }
