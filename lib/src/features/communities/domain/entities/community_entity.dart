55cc7dea960b32637fa40e154fef1946a80e441b
Lela Fieta <lelafieta11@gmail.com>
2025-02-25 18:54:09 +0100

    Update Community entity

--- lib/src/features/communities/domain/entities/community_entity.dart
+++ lib/src/features/communities/domain/entities/community_entity.dart
@@ -3,12 +3,16 @@ class CommunityEntity {
   final String? userId;
   final String? name;
   final String? description;
+  final String? image;
+  final String? banner;
   final DateTime? createdAt;
 
   CommunityEntity({
     this.id,
     this.userId,
     this.name,
+    this.image,
+    this.banner,
     this.description,
     this.createdAt,
   });
