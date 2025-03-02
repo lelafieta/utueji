ef6289cf46925de7dde9ad3e480a7eb804f8880c
Lela Fieta <lela.fieta@afrizona.co.ao>
2025-03-02 13:02:13 +0100

    Create MyCampaign Detail Page

---
+++ lib/src/features/campaigns/presentation/pages/my_campaign_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyCampaignDetailPage extends StatefulWidget {
+  const MyCampaignDetailPage({super.key});
+
+  @override
+  State<MyCampaignDetailPage> createState() => _MyCampaignDetailPageState();
+}
+
+class _MyCampaignDetailPageState extends State<MyCampaignDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
