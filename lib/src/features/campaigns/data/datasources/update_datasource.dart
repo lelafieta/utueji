1160f06ad7cfae7ae323fa4b585f68e9c0d280f8
Lela Fieta <lela.fieta@afrizona.co.ao>
2025-03-05 22:06:50 +0100

    implementing cubit

--- lib/src/features/campaigns/data/datasources/update_datasource.dart
+++ lib/src/features/campaigns/data/datasources/update_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:utueji/src/core/supabase/supabase_consts.dart';
 
 import '../models/campaign_update_model.dart';
 import 'i_update_datasource.dart';
@@ -10,9 +11,15 @@ class UpdateDataSource extends IUpdateDataSource {
   UpdateDataSource({required this.supabase});
 
   @override
-  Future<Unit> create(CampaignUpdateModel update) {
-    // TODO: implement create
-    throw UnimplementedError();
+  Future<Unit> create(CampaignUpdateModel update) async {
+    try {
+      final response =
+          await supabase.from(SupabaseConsts.updates).insert(update.toJson());
+
+      return response;
+    } catch (e) {
+      throw e;
+    }
   }
 
   @override
