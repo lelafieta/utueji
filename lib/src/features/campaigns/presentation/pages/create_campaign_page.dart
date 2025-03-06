f24db46f8460de8950d4e5fdd3d1aae89152d620
Lela Fieta <lela.fieta@afrizona.co.ao>
2025-03-06 21:58:05 +0100

    creating create campaign page

--- lib/src/features/campaigns/presentation/pages/create_campaign_page.dart
+++ lib/src/features/campaigns/presentation/pages/create_campaign_page.dart
@@ -1,4 +1,10 @@
+import 'package:easy_stepper/easy_stepper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:group_button/group_button.dart';
+
+import '../../../../config/themes/app_colors.dart';
 
 class CreateCampaignPage extends StatefulWidget {
   const CreateCampaignPage({super.key});
@@ -8,8 +14,176 @@ class CreateCampaignPage extends StatefulWidget {
 }
 
 class _CreateCampaignPageState extends State<CreateCampaignPage> {
+  int activeStep = 0;
+  int activeStep2 = 0;
+  int reachedStep = 0;
+  int upperBound = 5;
+  double progress = 0.2;
+  final _formKey = GlobalKey<FormState>();
+  String? _selectedOption;
+
+  List<String> titles = [
+    "Médico",
+    "Educação",
+    "Olfã",
+    "Animal",
+    "Idosos",
+    "Desporto",
+    "Desastre",
+    "Outros",
+  ];
+  Set<int> reachedSteps = <int>{0, 2, 4, 5};
+  final dashImages = [
+    'assets/1.png',
+    'assets/2.png',
+    'assets/3.png',
+    'assets/4.png',
+    'assets/5.png',
+  ];
+
+  void increaseProgress() {
+    if (progress < 1) {
+      setState(() => progress += 0.2);
+    } else {
+      setState(() => progress = 0);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Criar Nova Campanha'),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(16),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text("Guardar & Continuar"),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Container(
+                    width: 45,
+                    height: 45,
+                    color: Colors.black12,
+                  ),
+                  title: Text(
+                    "Passo 1/3",
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                  subtitle: Text(
+                    "Detalhes do Beneficiário",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        childAspectRatio: 4,
+                      ),
+                      itemCount: titles.length,
+                      itemBuilder: (context, index) {
+                        bool isSelected = _selectedOption == titles[index];
+
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _selectedOption = titles[index];
+                            });
+                          },
+                          child: Container(
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              color: isSelected
+                                  ? AppColors.primaryColor
+                                  : Colors.white,
+                              border: Border.all(
+                                color: AppColors.primaryColor,
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            alignment: Alignment.center,
+                            child: Text(
+                              titles[index],
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .copyWith(
+                                      color: isSelected
+                                          ? Colors.white
+                                          : Colors.black),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                    child: RichText(
+                      text: TextSpan(
+                        style: DefaultTextStyle.of(context).style,
+                        children: [
+                          TextSpan(
+                            text: "Para quem você arrecada fundos? ",
+                          ),
+                          TextSpan(
+                              text: "*",
+                              style: TextStyle(color: Colors.red, fontSize: 16))
+                        ],
+                      ),
+                    ),
+                  ),
+                  FormBuilderDropdown<String>(
+                    name: 'gender',
+                    initialValue: 'Mãe',
+                    decoration: InputDecoration(
+                      labelText: 'Gender',
+                      suffix: IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: () {
+                          // _formKey.currentState!.fields['gender']?.reset();
+                        },
+                      ),
+                      hintText: 'Select Gender',
+                    ),
+                    items: ["Mãe", "Pai", "Avô", "Irmão/a"]
+                        .map((gender) => DropdownMenuItem(
+                              alignment: AlignmentDirectional.center,
+                              value: gender,
+                              child: Text(gender),
+                            ))
+                        .toList(),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
